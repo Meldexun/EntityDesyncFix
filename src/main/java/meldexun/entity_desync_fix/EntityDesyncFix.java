17d2ab4fbc79444405eb84f1c9eb269e8c7c5cbb
Meldexun <27779321+Meldexun@users.noreply.github.com>
2022-06-03 16:45:41 +0200

    Make mod only being required server side

--- src/main/java/meldexun/entity_desync_fix/EntityDesyncFix.java
+++ src/main/java/meldexun/entity_desync_fix/EntityDesyncFix.java
@@ -2,7 +2,7 @@ package meldexun.entity_desync_fix;
 
 import net.minecraftforge.fml.common.Mod;
 
-@Mod(modid = EntityDesyncFix.MODID)
+@Mod(modid = EntityDesyncFix.MODID, acceptableRemoteVersions = "*")
 public class EntityDesyncFix {
 
 	public static final String MODID = "entity_desync_fix";
