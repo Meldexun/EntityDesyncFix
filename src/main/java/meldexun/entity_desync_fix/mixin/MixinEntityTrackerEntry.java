0d3853737803fb89e9153ef1d850a00546a7f37e
Meldexun <27779321+Meldexun@users.noreply.github.com>
2022-07-30 17:24:09 +0200

    Format comments

--- src/main/java/meldexun/entity_desync_fix/mixin/MixinEntityTrackerEntry.java
+++ src/main/java/meldexun/entity_desync_fix/mixin/MixinEntityTrackerEntry.java
@@ -13,49 +13,39 @@ import net.minecraft.entity.EntityTrackerEntry;
 @Mixin(EntityTrackerEntry.class)
 public class MixinEntityTrackerEntry {
 
-	/**
-	 * {@link EntityTrackerEntry#updatePlayerList(List)} at line 
-	 */
+	/** {@link EntityTrackerEntry#updatePlayerList(List)} */
 	@Redirect(method = "updatePlayerList", at = @At(value = "FIELD", target = "Lnet/minecraft/entity/Entity;posX:D"))
 	public double getPosX(Entity entity) {
 		return entity.prevPosX;
 	}
 
-	/**
-	 * {@link EntityTrackerEntry#updatePlayerList(List)} at line 
-	 */
+	/** {@link EntityTrackerEntry#updatePlayerList(List)} */
 	@Redirect(method = "updatePlayerList", at = @At(value = "FIELD", target = "Lnet/minecraft/entity/Entity;posY:D"))
 	public double getPosY(Entity entity) {
 		return entity.prevPosY;
 	}
 
-	/**
-	 * {@link EntityTrackerEntry#updatePlayerList(List)} at line 
-	 */
+	/** {@link EntityTrackerEntry#updatePlayerList(List)} */
 	@Redirect(method = "updatePlayerList", at = @At(value = "FIELD", target = "Lnet/minecraft/entity/Entity;posZ:D"))
 	public double getPosZ(Entity entity) {
 		return entity.prevPosZ;
 	}
 
-	/**
-	 * {@link EntityTrackerEntry#updatePlayerList(List)} at line 
-	 */
+
+
+	/** {@link EntityTrackerEntry#updatePlayerList(List)} */
 	@Redirect(method = "updatePlayerList", at = @At(value = "FIELD", target = "Lnet/minecraft/entity/Entity;motionX:D"))
 	public double getMotionX(Entity entity) {
 		return ((IPrevMotion) entity).getPrevMotionX();
 	}
 
-	/**
-	 * {@link EntityTrackerEntry#updatePlayerList(List)} at line 
-	 */
+	/** {@link EntityTrackerEntry#updatePlayerList(List)} */
 	@Redirect(method = "updatePlayerList", at = @At(value = "FIELD", target = "Lnet/minecraft/entity/Entity;motionY:D"))
 	public double getMotionY(Entity entity) {
 		return ((IPrevMotion) entity).getPrevMotionY();
 	}
 
-	/**
-	 * {@link EntityTrackerEntry#updatePlayerList(List)} at line 
-	 */
+	/** {@link EntityTrackerEntry#updatePlayerList(List)} */
 	@Redirect(method = "updatePlayerList", at = @At(value = "FIELD", target = "Lnet/minecraft/entity/Entity;motionZ:D"))
 	public double getMotionZ(Entity entity) {
 		return ((IPrevMotion) entity).getPrevMotionZ();
